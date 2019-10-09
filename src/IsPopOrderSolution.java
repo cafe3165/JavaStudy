fde1dc71df6348cca6bea4b2ea254bb303fbadf4
cafe3165 <78599297@qq.com>
2019-10-09 19:18:42 +0800

    刷题

---
+++ src/IsPopOrderSolution.java
@@ -0,0 +1,10 @@
+public class IsPopOrderSolution {
+    public static boolean IsPopOrder(int[] pushA, int[] popA) {
+        boolean f = false;
+        return f;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
