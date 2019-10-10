d17737c0d2b0a9044090073a3d81df77b4ae8108
cafe3165 <78599297@qq.com>
2019-10-10 20:53:44 +0800

    刷题

---
+++ src/MoreThanHalfNum_Solution.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class MoreThanHalfNum_Solution {
+    public int MoreThanHalfNum_Solution(int[] array) {
+        int num = 0, t = 0, count = 0;
+        Arrays.sort(array);
+        t = array[array.length / 2];
+
+        for (int i : array) {
+            if(i==t)
+                count++;
+        }
+        if(count>array.length / 2)
+            return t;
+        return num;
+
+    }
+
+    public static void main(String[] args) {
+//        int[] array = {1, 2, 3, 2, 2, 2, 5, 4, 2};
+//        int[] array = {0};
+        int[] array = {1, 2, 3, 2, 4, 2, 5, 2, 3};
+        MoreThanHalfNum_Solution m = new MoreThanHalfNum_Solution();
+        m.MoreThanHalfNum_Solution(array);
+    }
+}
