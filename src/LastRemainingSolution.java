a5a189cacf13dc3d5800a2e82f42f1b36142622e
cafe3165 <78599297@qq.com>
2019-10-16 20:12:59 +0800

    完成

--- src/LastRemainingSolution.java
+++ src/LastRemainingSolution.java
@@ -1,15 +1,23 @@
+import java.util.LinkedList;
+
 public class LastRemainingSolution {
     public int LastRemaining_Solution(int n, int m) {
-        int last=0;
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        for (int i = 0; i < n; i++)
+            list.add(i);
+        int tmp = 0;
+        while (list.size() > 1) {
+            tmp = (tmp + m - 1) % list.size();
+            list.remove(tmp);
+        }
 
-
-        return last;
+        return list.size() == 1 ? list.get(0) : -1;
 
     }
 
     public static void main(String[] args) {
-        LastRemainingSolution l=new LastRemainingSolution();
-        int n=5,m=3;
-        l.LastRemaining_Solution(n,m);
+        LastRemainingSolution l = new LastRemainingSolution();
+        int n = 5, m = 3;
+        l.LastRemaining_Solution(n, m);
     }
 }
