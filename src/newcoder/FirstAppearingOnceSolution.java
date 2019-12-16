f68107b6e219b719defd44090bd3f554428aa060
Joseph <78599297@qq.com>
2019-12-16 23:27:09 +0800

    字符流中第一个不重复的字符

--- src/newcoder/FirstAppearingOnceSolution.java
+++ src/newcoder/FirstAppearingOnceSolution.java
@@ -1,18 +1,52 @@
 package newcoder;
 
+import java.util.Map;
+import java.util.Iterator;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class FirstAppearingOnceSolution {
+    Map<Character, Integer> m = new LinkedHashMap<Character, Integer>();
 
     //Insert one char from stringstream
-    public void Insert(char ch)
-    {
+    public void Insert(char ch) {
+        if (m.containsKey(ch)) {
+            m.put(ch, m.get(ch) + 1);
+        } else {
+            m.put(ch, 1);
+        }
 
     }
+
     //return the first appearence once char in current stringstream
-    public char FirstAppearingOnce()
-    {
+    public char FirstAppearingOnce() {
 
+        Set<Map.Entry<Character, Integer>> entrySet = m.entrySet();
+        Iterator<Map.Entry<Character, Integer>> it = entrySet.iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = it.next();
+            Character k = (Character) entry.getKey();
+            Integer v = (Integer) entry.getValue();
+            if (v ==1) {
+                return k;
+
+            } else {
+                continue;
+            }
+        }
+        return '#';
     }
+
     public static void main(String[] args) {
+        FirstAppearingOnceSolution f = new FirstAppearingOnceSolution();
+        char[] chars = {'g', 'o', 'o', 'g', 'l', 'e'};
+        for (int i = 0; i < chars.length; i++) {
+            f.Insert(chars[i]);
+        }
+
+        System.out.println(f.FirstAppearingOnce());
 
     }
 }
