3dae57d42bed5387dbc385dbe598eca30e2515c4
cafe3165 <78599297@qq.com>
2019-12-16 20:37:17 +0800

    字符流

---
+++ src/newcoder/FirstAppearingOnceSolution.java
@@ -0,0 +1,18 @@
+package newcoder;
+
+public class FirstAppearingOnceSolution {
+
+    //Insert one char from stringstream
+    public void Insert(char ch)
+    {
+
+    }
+    //return the first appearence once char in current stringstream
+    public char FirstAppearingOnce()
+    {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
