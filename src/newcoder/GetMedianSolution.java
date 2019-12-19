e58e6980409c2107c6c1eed52d8f76411ca8f23b
cafe3165 <78599297@qq.com>
2019-12-19 18:14:32 +0800

    中位数

---
+++ src/newcoder/GetMedianSolution.java
@@ -0,0 +1,17 @@
+package newcoder;
+
+public class GetMedianSolution {
+    public void Insert(Integer num) {
+
+    }
+
+    public Double GetMedian() {
+
+        return 0.0;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
