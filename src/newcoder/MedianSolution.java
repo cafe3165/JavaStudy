0f67b49d0b790bbdf8fb59ba1b27fc1db82fa0b4
Joseph <78599297@qq.com>
2019-12-31 00:07:48 +0800

    牛客网 剑指offer一刷

---
+++ src/newcoder/MedianSolution.java
@@ -0,0 +1,41 @@
+package newcoder;
+
+import java.util.PriorityQueue;
+
+public class MedianSolution {
+
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
+    int count=0;
+
+    public void Insert(Integer num) {
+
+        if(count%2==0){
+            maxHeap.offer(num);
+            minHeap.offer(maxHeap.poll());
+        }else{
+            minHeap.offer(num);
+            maxHeap.offer(minHeap.poll());
+        }
+        count++;
+
+
+    }
+
+    public Double GetMedian() {
+        if(count%2==0){
+            return (maxHeap.peek()+minHeap.peek())/2.0;
+        }else{
+            return new Double(minHeap.peek());
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        MedianSolution m = new MedianSolution();
+
+    }
+
+
+}
