8b6deb9fa49efe99b4b758d583b96ffd36960c68
cafe3165 <78599297@qq.com>
2020-01-07 20:49:10 +0800

    原型模式

---
+++ src/designPattern/Protype/Main.java
@@ -0,0 +1,15 @@
+package designPattern.Protype;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+        Shape clonedShape = ShapeCache.getShape("1");
+        System.out.println("Shape : " + clonedShape.getType());
+
+        Shape clonedShape2 = ShapeCache.getShape("2");
+        System.out.println("Shape : " + clonedShape2.getType());
+
+        Shape clonedShape3 = ShapeCache.getShape("3");
+        System.out.println("Shape : " + clonedShape3.getType());
+    }
+}
