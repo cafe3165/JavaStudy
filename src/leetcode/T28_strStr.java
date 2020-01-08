c4ff1b2be80c27c5c696f8f91458716069d5b182
cafe3165 <78599297@qq.com>
2020-01-08 21:32:40 +0800

    2题

---
+++ src/leetcode/T28_strStr.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class T28_strStr {
+    public int strStr(String haystack, String needle) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        T28_strStr t = new T28_strStr();
+        String haystack = "hello", needle = "ll";
+//      Sunday算法
+        t.strStr(haystack, needle);
+
+    }
+}
