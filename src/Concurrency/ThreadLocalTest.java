f23511dfb74f95e8296e5c9b7ed7ac046fad6041
cafe3165 <78599297@qq.com>
2020-04-13 17:23:48 +0800

    修改

--- src/Concurrency/ThreadLocalTest.java
+++ src/Concurrency/ThreadLocalTest.java
@@ -12,30 +12,29 @@ import java.util.concurrent.*;
  */
 public class ThreadLocalTest {
     static ThreadLocal<String> localVar = new ThreadLocal<>();
-    public static void print(String threadname){
-        System.out.println(threadname+localVar.get());
+
+    public static void print(String threadname) {
+        System.out.println(threadname + localVar.get());
         localVar.remove();
     }
 
     public static void main(String[] args) {
         ThreadFactory tf = new ThreadFactoryBuilder().setNameFormat("thread-pool-%d").build();
-        ExecutorService es = new ThreadPoolExecutor(1, 10, 300, TimeUnit.MILLISECONDS, new SynchronousQueue<Runnable>(), tf, new ThreadPoolExecutor.AbortPolicy());
-        es.execute(() -> {
-            localVar.set("11");
-            print(Thread.currentThread().getName());
-            System.out.println(localVar.get());
-//            print(Thread);
-        });
-        es.execute(() -> {
-            localVar.set("222");
-            print(Thread.currentThread().getName());
-            System.out.println(localVar.get());
-
-        });
-
-
-
-
+        ExecutorService es = new ThreadPoolExecutor(1, 3, 300, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(3), tf, new ThreadPoolExecutor.DiscardOldestPolicy());
+//       ExecutorService ee=new ThreadPoolExecutor(1)
+//        es.execute(() -> {
+//            localVar.set("11");
+//            print(Thread.currentThread().getName());
+//            System.out.println(localVar.get());
+////            print(Thread);
+//        });
+//        es.execute(() -> {
+//            localVar.set("222");
+//            print(Thread.currentThread().getName());
+//            System.out.println(localVar.get());
+//
+//        });
+//        es.execute(new Thread({}));
     }
 
 
--- src/Concurrency/countdownlatchtest_oddeven.java
+++ src/Concurrency/countdownlatchtest_oddeven.java
@@ -15,39 +15,32 @@ public class countdownlatchtest_oddeven {
 
     public static void main(String[] args) throws InterruptedException {
         ThreadFactory tf = new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build();
-        ExecutorService es = new ThreadPoolExecutor(2, 10, 300, TimeUnit.MILLISECONDS, new SynchronousQueue<Runnable>(), tf, new ThreadPoolExecutor.AbortPolicy());
+        ExecutorService es = new ThreadPoolExecutor(1, 2, 300, TimeUnit.MILLISECONDS, new SynchronousQueue<Runnable>(), tf, new ThreadPoolExecutor.AbortPolicy());
 
 
-        es.execute(new Runnable() {
-
-            @Override
-            public void run() {
-                while (num.intValue() < 100) {
-                    if (num.intValue() % 2 == 0) {
-                        System.out.println(Thread.currentThread().getName() + "偶数线程"+num.intValue());
-                        num.incrementAndGet();
-                    }
-                    count.countDown();
+        es.execute(() -> {
+            while (num.intValue() < 10) {
+                if (num.intValue() % 2 == 0) {
+                    System.out.println(Thread.currentThread().getName() + "偶数线程"+num.intValue());
+                    num.incrementAndGet();
                 }
+                count.countDown();
             }
         });
 
-        es.execute(new Runnable() {
-
-            @Override
-            public void run() {
-                while (num.intValue() < 100) {
-                    if (num.intValue() % 2 != 0) {
-                        System.out.println(Thread.currentThread().getName() + "奇数线程"+num.intValue());
-                        num.incrementAndGet();
-                    }
-                    count.countDown();
+        es.execute(() -> {
+            while (num.intValue() < 10) {
+                if (num.intValue() % 2 != 0) {
+                    System.out.println(Thread.currentThread().getName() + "奇数线程"+num.intValue());
+                    num.incrementAndGet();
                 }
+                count.countDown();
             }
         });
 
         count.await();
         es.shutdown();
+
 
     }
 
