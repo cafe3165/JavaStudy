43da1e91d273109bf71033c202a8bdbd7913e28e
cafe3165 <78599297@qq.com>
2020-05-10 09:34:42 +0800

    java 8新特性测试

--- src/J2SE/java8/Streamtest.java
+++ src/J2SE/java8/Streamtest.java
@@ -1,5 +1,9 @@
 package J2SE.java8;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -8,9 +12,14 @@ import java.util.stream.Stream;
  */
 public class Streamtest {
     public static void main(String[] args) {
-        String str = "my name is 007";
-
-        Stream.of(str.split(" ")).filter(s -> s.length() > 2)
-                .map(s -> s.length()).forEach(System.out::println);
+//        String str = "my name is 007";
+//
+//        Stream.of(str.split(" ")).filter(s -> s.length() > 2)
+//                .map(s -> s.length()).forEach(System.out::println);
+        List<Integer> ls = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            ls.add(i);
+        }
+        ls.stream().sorted((a,b)->(b-a)).filter((s) -> s > 5).forEach(System.out::println);
     }
 }
---
+++ src/J2SE/java8/interfaceDemo.java
@@ -0,0 +1,10 @@
+package J2SE.java8;
+
+/**
+ * Author:cafe3165
+ * Date:2020-05-03
+ */
+@FunctionalInterface
+public interface interfaceDemo {
+    void f();
+}
