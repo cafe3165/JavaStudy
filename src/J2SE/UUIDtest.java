85ac838a8270e92fa32112ee676fd87b48e9a23c
cafe3165 <78599297@qq.com>
2020-05-23 09:10:05 +0800

    UUID生成

---
+++ src/J2SE/UUIDtest.java
@@ -0,0 +1,15 @@
+package J2SE;
+
+import java.util.UUID;
+
+/**
+ * Author:cafe3165
+ * Date:2020-05-21
+ */
+public class UUIDtest {
+    public static void main(String[] args) {
+        UUID uuid=UUID.randomUUID();
+        System.out.println(uuid.toString());
+
+    }
+}
