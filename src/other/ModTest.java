a1bc668d4e35f5125db32b24db5b5b62179d3900
cafe3165 <78599297@qq.com>
2020-05-28 10:22:32 +0800

    负数模运算

---
+++ src/other/ModTest.java
@@ -0,0 +1,14 @@
+package other;
+
+
+/**
+ * Author:cafe3165
+ * Date:2020-05-27
+ */
+public class ModTest {
+    public static void main(String[] args) {
+        int m = -2;
+        int k = 5;
+        System.out.println((m % k + k) % k);
+    }
+}
