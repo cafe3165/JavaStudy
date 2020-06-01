4fecab7ad95a1c71dfea56c284670dfeb8bc840c
cafe3165 <78599297@qq.com>
2020-06-01 15:22:07 +0800

    排列组合：溢出 简单dp

---
+++ src/leetcode_list200/T62uniquePaths.java
@@ -0,0 +1,33 @@
+package leetcode_list200;
+
+/**
+ * Author:cafe3165
+ * Date:2020-06-01
+ */
+public class T62uniquePaths {
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+
+
+    }
+
+
+    public static void main(String[] args) {
+        int m = 7, n = 3;
+        System.out.println(uniquePaths(m, n));
+
+    }
+}
