dc1626d045df6702c2a8387c824b510bf10f2c8b
cafe3165 <78599297@qq.com>
2020-06-27 15:27:28 +0800

    无

--- src/other/Stringtest.java
+++ src/other/Stringtest.java
@@ -11,12 +11,17 @@ public class Stringtest {
     public static void main(String[] args) {
 //        String str=new String("abc");
 //        System.out.println(str);
-        Integer[] aa = {3, 2, 23, 543, 2, 3, 1};
-        List<Integer>l=new ArrayList<>();
-      Collections.addAll(l,aa);
-      Collections.sort(l,(a,b)->b-a);
-      Collections.rotate(l,-3);
-        System.out.println(l);
-
+//        Integer[] aa = {3, 2, 23, 543, 2, 3, 1};
+//        List<Integer>l=new ArrayList<>();
+//      Collections.addAll(l,aa);
+//      Collections.sort(l,(a,b)->b-a);
+//      Collections.rotate(l,-3);
+//        System.out.println(l);
+        String a = "abc";
+//        byte[] aa=a.getBytes();
+        StringBuffer sb = new StringBuffer("abc");
+        System.out.println(a.contentEquals(sb));
+        char[] aa = {'s', 's'};
+        System.out.println(aa.length);
     }
 }
