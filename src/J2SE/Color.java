bd1e4ccfde29065e5016a17a213b8b045d3b2c02
cafe3165 <78599297@qq.com>
2020-07-03 14:34:36 +0800

    枚举赋值

---
+++ src/J2SE/Color.java
@@ -0,0 +1,50 @@
+package J2SE;
+
+public enum Color {
+    RED("红色", 1), GREEN("绿色", 2), BLANK("白色", 3), YELLOW("黄色", 4);
+    //  成员变量
+    private String name;
+    private int index;
+
+    //  构造方法
+    Color(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+    //  普通方法
+    public static String getName(int index) {
+        for (Color c : Color.values()) {
+            if (c.getIndex() == index) {
+                return c.name;
+            }
+        }
+        return null;
+    }
+
+    //  get  set  方法
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println( Color.getName(1));
+
+    }
+
+
+}
