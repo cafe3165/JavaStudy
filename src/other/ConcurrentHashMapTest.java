6d403152a445a086d11d6cf8dbe33a7ff83e86c7
cafe3165 <78599297@qq.com>
2020-10-15 17:08:43 +0800

    源码解析

---
+++ src/other/ConcurrentHashMapTest.java
@@ -0,0 +1,19 @@
+package other;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Author:cafe3165
+ * Date:2020-10-15
+ */
+public class ConcurrentHashMapTest {
+    public static void main(String[] args) {
+        Hashtable<String, String> hashtable = new Hashtable<>();
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>(4);
+        HashMap<String, String> hashMap = new HashMap<>();
+
+
+    }
+}
