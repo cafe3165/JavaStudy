3325461749604264479b312dade5867f03d6164a
huangzhiming04 <huangzhiming04@meituan.com>
2022-06-18 21:33:25 +0800

    更改目录

---
+++ leetcode/src/test/java/com/example/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
